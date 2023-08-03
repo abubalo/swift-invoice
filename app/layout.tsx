dc89ec4234d5621e8af65bcde9abd2b71a296e0d
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-08-03 19:21:14 +0200

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="shortcut icon" href="/public/logo.svg" type="image/x-icon" />
       <body>
         <Header />
         {children}
