b79b61212935e58a29119513f7be7198c0b65d52
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-08-08 23:34:00 +0200

    Refactor dasboard page by add dedicated layout

---
+++ app/(dashboard)/generate/page.tsx
@@ -0,0 +1,12 @@
+import Invoice from '@/components/invoice/Invoice'
+import React from 'react'
+
+const Generate = () => {
+  return (
+    <div>
+        <Invoice />
+    </div>
+  )
+}
+
+export default Generate
