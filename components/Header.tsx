79c6bb28a8a8b218db8a4862a5d0f71cea729487
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-08-08 23:36:57 +0200

    Add pricing path to pricing anchor tag

--- components/Header.tsx
+++ components/Header.tsx
@@ -58,7 +58,7 @@ const Header = () => {
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 ">
               <li>
                 <Link
-                  href="#"
+                  href="/pricing"
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Pricing
