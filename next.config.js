a3df3aa2a6a69c7f4b802836d48f910b2602e11a
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-08-08 23:35:55 +0200

    Add unsplash base url

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['images.unsplash.com'] // Add the domain of the image source here
+      },
+}
 
 module.exports = nextConfig
