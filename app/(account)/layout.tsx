0771eb141c880c899da80293dbab1ae7b6672b80
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-08-08 23:35:20 +0200

    Group login sign and profile into account

---
+++ app/(account)/layout.tsx
@@ -0,0 +1,22 @@
+import "../globals.css";
+import Header from "@/components/Header";
+
+export const metadata = {
+  title: "Swift invoice",
+  description: "Generate invoice on the go",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <link rel="shortcut icon" href="/logo.svg" type="image/x-icon" />
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
