7bf339aa121423490a74080ad22a6df15ba70db4
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-08-11 00:56:35 +0200

    style:  make dasboard navigation responsive

--- app/(dashboard)/components/Nav.tsx
+++ app/(dashboard)/components/Nav.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <nav className="flex justify-between items-center">
-      <ul className="w-max flex bg-gray-900 p-2 rounded-md">
+    <nav className="flex justify-between items-center ">
+      <ul className="hidden w-max  bg-gray-900 p-2 rounded-md md:flex">
         <li className="bg-black px-4 py-2 rounded-lg hover:bg-gray-950 transition-all ease-linear">
           <Link href={"#"}>Oveview</Link>
         </li>
@@ -22,7 +22,35 @@ const Nav = () => {
         </li>
       </ul>
 
-      <Link href={"/invoice"} className="px-4 py-2 bg-primary rounded-lg hover:bg-primary/90">
+      <button
+        data-collapse-toggle="navbar-cta"
+        type="button"
+        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-600 dark:hover:bg-gray-800 dark:focus:ring-gray-900"
+        aria-controls="navbar-cta"
+        aria-expanded="false"
+      >
+        <span className="sr-only">Open main menu</span>
+        <svg
+          className="w-5 h-5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 17 14"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M1 1h15M1 7h15M1 13h15"
+          />
+        </svg>
+      </button>
+
+      <Link
+        href={"/invoice"}
+        className="px-4 py-2 bg-primary rounded-lg hover:bg-primary/90"
+      >
         Create Invoice
       </Link>
     </nav>
