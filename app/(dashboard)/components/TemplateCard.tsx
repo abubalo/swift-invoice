286c8c3a182c562a19adf3ccb5145754c3ed6bfd
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-08-10 22:37:19 +0200

    Add border backgroud effect to template card

--- app/(dashboard)/components/TemplateCard.tsx
+++ app/(dashboard)/components/TemplateCard.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
 import React from "react";
 
 const TemplateCard = () => {
   return (
-    <div className="flex flex-col justify-between bg-primary p-8 rounded-md">
+    <div className="relative flex flex-col justify-between bg-primary p-8 rounded-md overflow-hidden">
       <div className="space-y-6">
         <p className="w-max py-1 px-3 text-sm text-black bg-white rounded-full">
           New
@@ -15,7 +15,14 @@ const TemplateCard = () => {
           grow
         </p>
       </div>
-      <button className="font-semibold p-4 text-lg text-primary bg-white  rounded-lg">Download</button>
+      <button className="font-semibold p-4 text-lg text-primary bg-white  rounded-lg z-10">
+        Download
+      </button>
+      <div className="absolute w-[320px] h-[320px] border border-white/80 -bottom-24 -right-8 rounded-tl-[200px] rounded-tr-[60px]"></div>
+      <div className="absolute w-[300px] h-[300px] border border-gray-300 -bottom-24 -right-8 rounded-tl-[200px] rounded-tr-[60px]"></div>
+      <div className="absolute w-[280px] h-[280px] border border-gray-300 -bottom-24 -right-8 rounded-tl-[200px] rounded-tr-[60px]"></div>
+      <div className="absolute w-[240px] h-[240px] border border-gray-300 -bottom-24 -right-8 rounded-tl-[200px] rounded-tr-[60px]"></div>
+      <div className="absolute w-[200px] h-[200px] border border-gray-300 -bottom-24 -right-8 rounded-tl-[200px] rounded-tr-[60px]"></div>
     </div>
   );
 };
