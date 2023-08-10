d5a4f26f7817720d3f2e54672f49017bd7d2b895
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-08-11 00:07:13 +0200

    Renamed profile direcoty to account

---
+++ app/(account)/account/layout.tsx
@@ -0,0 +1,21 @@
+import "../globals.css";
+
+export const metadata = {
+  title: "Swift invoice",
+  description: "Generate invoice on the go",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <link rel="shortcut icon" href="/logo.svg" type="image/x-icon" />
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
