46f1f270497e809b32763d5e4c58d96d0203c9e7
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-08-11 00:58:07 +0200

    style: add responsiveness to activity section

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -53,7 +53,7 @@ const Dashboard = () => {
             <EarningsChart />
             <TemplateCard />
           </div>
-          <div className="flex gap-5 justify-between">
+          <div className="flex flex-col gap-5 justify-between lg:flex-row">
             <Activites />
             <TransactionHistory />
           </div>
