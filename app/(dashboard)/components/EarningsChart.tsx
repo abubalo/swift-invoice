df4b4a89f840e0068266a4b46d9165c70366ce92
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-08-10 22:36:22 +0200

    Add padding and border to Earning chart component

--- app/(dashboard)/components/EarningsChart.tsx
+++ app/(dashboard)/components/EarningsChart.tsx
@@ -36,7 +36,7 @@ const EarningsChart = () => {
   }));
 
   return (
-    <ResponsiveContainer width="100%" height={400}>
+    <ResponsiveContainer width="100%" height={400} className="py-2 border border-gray-700 rounded-md">
       <BarChart data={monthlyEarningsData}>
         <XAxis
           dataKey="month"
