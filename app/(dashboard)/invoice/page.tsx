72ec3fe382c6af20212114e91d757bc3355ea594
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-08-11 23:34:00 +0200

    feat: add invoice page

---
+++ app/(dashboard)/invoice/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import React from 'react'
+import InvoiceTemplate from '../components/InvoiceTemplate'
+import InvoiceForm from '../components/InvoiceForm'
+
+const Generate = () => {
+  
+  return (
+    <section>
+        {/* <InvoiceTemplate /> */}
+        <InvoiceForm />
+    </section>
+  )
+}
+
+export default Generate
