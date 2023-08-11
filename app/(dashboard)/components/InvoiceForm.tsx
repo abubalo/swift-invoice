081ae7f1958d9cb8320de3310d265ffe7a67cd09
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-08-11 23:15:44 +0200

    feat: Implement Formik and FieldArray in InvoiceForm

--- app/(dashboard)/components/InvoiceForm.tsx
+++ app/(dashboard)/components/InvoiceForm.tsx
@@ -1,8 +1,14 @@
-"use client"
+"use client";
 
 import React from "react";
 import ServiceForm from "./ServiceForm";
-import { FieldArray, useFormik, FieldArrayRenderProps } from "formik";
+import {
+  FieldArray,
+  useFormik,
+  FieldArrayRenderProps,
+  Formik,
+  Form,
+} from "formik";
 
 type Company = {
   companyName: string;
@@ -31,223 +37,232 @@ type InitialValues = {
 };
 
 const InvoiceForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      company: {
-        companyName: "",
-        address: "",
-        phone: "",
-        email: "",
-      },
-      customer: {
-        billTo: "",
-        customerName: "",
-        customerAddress: "",
-        customerEmail: "",
-      },
-      services: [
-        {
-          serviceName: "",
-          quantity: 1,
-          price: 0,
-        },
-      ],
+  const initialValues = {
+    company: {
+      companyName: "",
+      address: "",
+      phone: "",
+      email: "",
     },
-
-    onSubmit: (values: InitialValues) => {},
-  });
+    customer: {
+      billTo: "",
+      customerName: "",
+      customerAddress: "",
+      customerEmail: "",
+    },
+    services: [
+      {
+        serviceName: "",
+        quantity: 1,
+        price: 0,
+      },
+    ],
+  };
 
   // const addServiceForm = (values: InitialValues) => {
-  //   values.services.push({
+  //   formik.values.services.push({
   //     serviceName: "",
   //     quantity: 1,
   //     price: 0,
   //   });
   // };
 
+  const onSubmit = (data: InitialValues) => {
+    console.log(data);
+  };
   return (
     <div className="min-h-screen flex justify-center items-center">
       <div className="border border-gray-800 p-4 rounded-lg ">
         <h1 className="text-2xl font-semibold text-center my-3">
           Invoice Form
         </h1>
+        <Formik initialValues={initialValues} onSubmit={onSubmit}>
+          {(formik) => {
+            return (
+              <Form className="space-y-3">
+                <div className="w-full flex gap-5 ">
+                  <div className="w-full space-y-3">
+                    <div id="" className="space-y-1">
+                      <label htmlFor="CompanyName">Company Name</label>
+                      <input
+                        className="appearance-none border rounded w-full py-2 px-3 text-gray-500 bg-gray-800 leading-tight focus:outline-none"
+                        type="text"
+                        id="CompanyName"
+                        name="CompanyName"
+                        placeholder="Your Comapany or Business Name"
+                        value={formik.values.company.companyName}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        required
+                        aria-required="true"
+                        aria-label="Your Comapany or Business Name"
+                      />
+                    </div>
+                    <div id="" className="space-y-1">
+                      <label htmlFor="address">Address</label>
+                      <input
+                        className="appearance-none border rounded w-full py-2 px-3 text-gray-500 bg-gray-800 leading-tight focus:outline-none"
+                        type="text"
+                        id="address"
+                        name="address"
+                        placeholder="Your Comapany Address"
+                        value={formik.values.company.address}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        required
+                        aria-required="true"
+                        aria-label="Your Comapany Address"
+                      />
+                    </div>
+                    <div id="" className="space-y-1">
+                      <label htmlFor="phone">Phone Number</label>
+                      <input
+                        className="appearance-none border rounded w-full py-2 px-3 text-gray-500 bg-gray-800 leading-tight focus:outline-none"
+                        type="text"
+                        id="phone"
+                        name="phone"
+                        placeholder="Phone number"
+                        value={formik.values.company.phone}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        required
+                        aria-required="true"
+                        aria-label="Comapany Phone Number"
+                      />
+                    </div>
+                    <div id="" className="space-y-1">
+                      <label htmlFor="email">Email</label>
+                      <input
+                        className="appearance-none border rounded w-full py-2 px-3 text-gray-500 bg-gray-800 leading-tight focus:outline-none"
+                        type="text"
+                        id="email"
+                        name="email"
+                        placeholder="Email address"
+                        value={formik.values.company.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        required
+                        aria-required="true"
+                        aria-label="Email address"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full space-y-3">
+                    <div id="customerAddress" className="">
+                      <label htmlFor="billTo">Bill To</label>
+                      <input
+                        className="appearance-none border rounded w-full py-2 px-3 text-gray-500 bg-gray-800 leading-tight focus:outline-none"
+                        type="text"
+                        id="BillTo"
+                        name="BillTo"
+                        placeholder="Address"
+                        value={formik.values.customer.billTo}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        required
+                        aria-required="true"
+                        aria-label="Customer address"
+                      />
+                    </div>
+                    <div id="" className="space-y-1">
+                      <label htmlFor="customerName">Customer Name</label>
+                      <input
+                        className="appearance-none border rounded w-full py-2 px-3 text-gray-500 bg-gray-800 leading-tight focus:outline-none"
+                        type="text"
+                        id="customerName"
+                        name="customerName"
+                        placeholder="Costomer name"
+                        value={formik.values.customer.customerName}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        required
+                        aria-required="true"
+                        aria-label="Customer Name"
+                      />
+                    </div>
+                    <div id="" className="space-y-1">
+                      <label htmlFor="address">Address</label>
+                      <input
+                        className="appearance-none border rounded w-full py-2 px-3 text-gray-500 bg-gray-800 leading-tight focus:outline-none"
+                        type="text"
+                        id="address"
+                        name="address"
+                        placeholder="Your Comapany Address"
+                        value={formik.values.customer.customerAddress}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        required
+                        aria-required="true"
+                        aria-label="Your Comapany Address"
+                      />
+                    </div>
+                    <div id="" className="space-y-1">
+                      <label htmlFor="email">Email</label>
+                      <input
+                        className="appearance-none border rounded w-full py-2 px-3 text-gray-500 bg-gray-800 leading-tight focus:outline-none"
+                        type="text"
+                        id="email"
+                        name="email"
+                        placeholder="Email address"
+                        value={formik.values.customer.customerEmail}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        required
+                        aria-required="true"
+                        aria-label="Email address"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <FieldArray name="services">
+                  {(arrayHelpers: FieldArrayRenderProps) => (
+                    <div>
+                      {/* Map through services and render ServiceForm */}
+                      {formik.values.services.map((service, index) => (
+                        <ServiceForm
+                          key={index}
+                          index={index}
+                          service={service}
+                          handleChange={formik.handleChange}
+                          remove={() => arrayHelpers.remove(index)}
+                        />
+                      ))}
+                      {/* Add new service button */}
+                      <button
+                        type="button"
+                        className="py-2 px-4 bg-gray-950 hover:bg-gray-600"
+                        onClick={() => {
+                          // Insert a new service object
+                          arrayHelpers.push({
+                            serviceName: "",
+                            quantity: 1,
+                            price: 0,
+                          });
+                        }}
+                      >
+                        Add new item +
+                      </button>
+                    </div>
+                  )}
+                </FieldArray>
 
-        <form
-          action=""
-          onSubmit={formik.handleSubmit}
-          aria-label="Contact Us Form"
-          className="space-y-3"
-        >
-          <div className="w-full flex gap-5 ">
-            <div className="w-full space-y-3">
-              <div id="" className="space-y-1">
-                <label htmlFor="CompanyName">Company Name</label>
-                <input
-                  className="appearance-none border rounded w-full py-2 px-3 text-gray-500 bg-gray-800 leading-tight focus:outline-none"
-                  type="text"
-                  id="CompanyName"
-                  name="CompanyName"
-                  placeholder="Your Comapany or Business Name"
-                  value={formik.values.company.companyName}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  required
-                  aria-required="true"
-                  aria-label="Your Comapany or Business Name"
-                />
-              </div>
-              <div id="" className="space-y-1">
-                <label htmlFor="address">Address</label>
-                <input
-                  className="appearance-none border rounded w-full py-2 px-3 text-gray-500 bg-gray-800 leading-tight focus:outline-none"
-                  type="text"
-                  id="address"
-                  name="address"
-                  placeholder="Your Comapany Address"
-                  value={formik.values.company.address}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  required
-                  aria-required="true"
-                  aria-label="Your Comapany Address"
-                />
-              </div>
-              <div id="" className="space-y-1">
-                <label htmlFor="phone">Phone Number</label>
-                <input
-                  className="appearance-none border rounded w-full py-2 px-3 text-gray-500 bg-gray-800 leading-tight focus:outline-none"
-                  type="text"
-                  id="phone"
-                  name="phone"
-                  placeholder="Phone number"
-                  value={formik.values.company.phone}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  required
-                  aria-required="true"
-                  aria-label="Comapany Phone Number"
-                />
-              </div>
-              <div id="" className="space-y-1">
-                <label htmlFor="email">Email</label>
-                <input
-                  className="appearance-none border rounded w-full py-2 px-3 text-gray-500 bg-gray-800 leading-tight focus:outline-none"
-                  type="text"
-                  id="email"
-                  name="email"
-                  placeholder="Email address"
-                  value={formik.values.company.email}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  required
-                  aria-required="true"
-                  aria-label="Email address"
-                />
-              </div>
-            </div>
-            <div className="w-full space-y-3">
-              <div id="customerAddress" className="">
-                <label htmlFor="billTo">Bill To</label>
-                <input
-                  className="appearance-none border rounded w-full py-2 px-3 text-gray-500 bg-gray-800 leading-tight focus:outline-none"
-                  type="text"
-                  id="BillTo"
-                  name="BillTo"
-                  placeholder="Address"
-                  value={formik.values.customer.billTo}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  required
-                  aria-required="true"
-                  aria-label="Customer address"
-                />
-              </div>
-              <div id="" className="space-y-1">
-                <label htmlFor="customerName">Customer Name</label>
-                <input
-                  className="appearance-none border rounded w-full py-2 px-3 text-gray-500 bg-gray-800 leading-tight focus:outline-none"
-                  type="text"
-                  id="customerName"
-                  name="customerName"
-                  placeholder="Costomer name"
-                  value={formik.values.customer.customerName}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  required
-                  aria-required="true"
-                  aria-label="Customer Name"
-                />
-              </div>
-              <div id="" className="space-y-1">
-                <label htmlFor="address">Address</label>
-                <input
-                  className="appearance-none border rounded w-full py-2 px-3 text-gray-500 bg-gray-800 leading-tight focus:outline-none"
-                  type="text"
-                  id="address"
-                  name="address"
-                  placeholder="Your Comapany Address"
-                  value={formik.values.customer.customerAddress}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  required
-                  aria-required="true"
-                  aria-label="Your Comapany Address"
-                />
-              </div>
-              <div id="" className="space-y-1">
-                <label htmlFor="email">Email</label>
-                <input
-                  className="appearance-none border rounded w-full py-2 px-3 text-gray-500 bg-gray-800 leading-tight focus:outline-none"
-                  type="text"
-                  id="email"
-                  name="email"
-                  placeholder="Email address"
-                  value={formik.values.customer.customerEmail}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  required
-                  aria-required="true"
-                  aria-label="Email address"
-                />
-              </div>
-            </div>
-          </div>
-          <FieldArray name="services">
-            {(arrayHelplers: FieldArrayRenderProps) => (
-              <div>
-                {formik.values.services.map((service, index) => (
-                  <ServiceForm
-                    key={index}
-                    index={index}
-                    service={service}
-                    handleChange={formik.handleChange}
-                    remove={() => arrayHelplers.remove(index)}
-                  />
-                ))}
-                <button
-                  type="button"
-                  className="py-2 px-4 bg-gray-950 hover:bg-gray-600"
-                  onClick={() => {
-                    arrayHelplers.push({
-                      serviceName: "",
-                      quantity: 1,
-                      price: 0,
-                    });
-                  }}
-                >
-                  Add new item +
-                </button>
-              </div>
-            )}
-          </FieldArray>
-          <div>
-            <button type="submit">Save draft</button>
-            <button type="submit">Submit</button>
-          </div>
-        </form>
+                <div>
+                  <button type="submit">Save draft</button>
+                  <button type="submit">Submit</button>
+                </div>
+              </Form>
+            );
+          }}
+        </Formik>
       </div>
     </div>
   );
 };
 
 export default InvoiceForm;
+function index(
+  index: any,
+  arg1: { serviceName: string; quantity: number; price: number }
+) {
+  throw new Error("Function not implemented.");
+}
