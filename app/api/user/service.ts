93808b733ae033a3f779d246e13d746a05c4f64c
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-08-12 18:56:53 +0200

    feat: add user service

---
+++ app/api/user/service.ts
@@ -0,0 +1,112 @@
+import { UserDocument } from "@/app/types/types";
+import UserModel from "./model";
+import bcrypt from "bcrypt";
+import Jwt from "jsonwebtoken";
+
+export const addUser = async (
+  name: string,
+  phone: number,
+  email: string,
+  password: string
+): Promise<UserDocument> => {
+  try {
+    const saltRounds = 10;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const newUser = await UserModel.create({
+      name,
+      phone,
+      email,
+      password: hashedPassword,
+    });
+
+    return newUser;
+  } catch (error: any) {
+    throw new Error(`Error adding user: ${error.message}`);
+  }
+};
+
+export const authenticate = async (
+  email: string,
+  password: string
+): Promise<UserDocument | null> => {
+  const user = await UserModel.findOne({ email });
+
+  if (!user) {
+    console.log("User does not exist");
+    return null;
+  }
+
+  const passwordMatches = bcrypt.compareSync(password, user.password);
+
+  if (!passwordMatches) {
+    console.log("Passsword doas not match");
+    return null;
+  }
+
+  return user; // Return the found user document, or null if not found
+};
+
+export const selectUser = async (id: string): Promise<UserDocument | null> => {
+  try {
+    const user = await UserModel.findById(id);
+    return user;
+  } catch (error: any) {
+    throw new Error(`Error selecting user: ${error.message}`);
+  }
+};
+
+export const updateUser = async (
+  id: string,
+  newData: object
+): Promise<Boolean | null> => {
+  const user = await UserModel.findByIdAndUpdate(id, newData);
+
+  if (user) return true;
+
+  return null;
+};
+
+export const deleteUser = async (id: string): Promise<Boolean> => {
+  return true;
+};
+
+export const verifyUser = async (email: string): Promise<Boolean> => {
+  const isUserExist = await UserModel.findOne({ email });
+  // return !!isUserExist; // Return true if the user exists, otherwise false
+  if (!isUserExist) return false;
+
+  return true;
+};
+
+export const generateAccessToken = async (user: string): Promise<string> => {
+  const jwtSecret = process.env.JWT_SECRET;
+  if (!jwtSecret) throw new Error("JWT is not provided");
+
+  const token = await Jwt.sign({ sub: user }, jwtSecret, {});
+  return token;
+};
+
+
+
+export const verifyAccessToken = async (
+    accessToken: string
+): Promise<UserDocument | null> => {
+  const jwtSecret = process.env.JWT_SECRET;
+  if (!jwtSecret) throw new Error("JWT is not provided");
+
+  try {
+    const decoded = Jwt.verify(accessToken, jwtSecret) as {userId: string};
+
+    const userId = decoded.userId;
+    const user = await UserModel.findOne({userId});
+
+    if(!user) return null;
+
+    return user;
+  } catch (error: any) {
+    console.log({error: error.message});
+    return null;
+  }
+};
