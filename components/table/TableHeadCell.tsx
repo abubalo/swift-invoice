cfeaa4ee820665dd8d9342a55bfacdfc44625b43
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-08-15 19:31:06 +0200

    refactor: delete unwanted  table components

--- components/table/TableHeadCell.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-type Props={
-    className?: string;
-}
-const TableHeadCell = ({className}: Props) => <th className={className}>TableHeadCell</th>
-  
-
-export default TableHeadCell
