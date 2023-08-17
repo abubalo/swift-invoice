c7c8e6bc53d368834bc5da5d425b0fb1b16b15e4
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-08-17 20:12:48 +0200

    Change content text

--- app/(dashboard)/components/TemplateCard.tsx
+++ app/(dashboard)/components/TemplateCard.tsx
@@ -4,15 +4,14 @@ import React from "react";
 
 const TemplateCard = () => {
   return (
-    <div className="relative flex flex-col justify-between bg-primary p-8 rounded-md overflow-hidden">
+    <div className="basis-2/5 relative flex flex-col justify-between bg-primary p-8 rounded-md overflow-hidden">
       <div className="space-y-6">
         <p className="w-max py-1 px-3 text-sm text-black bg-white rounded-full">
           New
         </p>
-        <h1 className="text-2xl font-medium">We have a new invoice template</h1>
+        <h1 className="text-2xl font-medium">Introducing our latest invoice template</h1>
         <p>
-          This new template provides more personalize way to help your bussness
-          grow
+        Designed to fuel the growth of your business, this cutting-edge template offers a highly personalized approach that sets the stage for your success.
         </p>
       </div>
       <button className="font-semibold p-4 text-lg text-primary bg-white  rounded-lg z-10">
