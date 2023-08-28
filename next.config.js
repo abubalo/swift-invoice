de4c8656a7b014772c316c4889eab94e12f69b22
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-08-28 21:45:39 +0200

    add serverComponentsExternalPackages setting

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com'] // Add the domain of the image source here
-      },
-}
+  images: {
+    domains: ["images.unsplash.com"], // Add the domain of the image source here
+  },
+  experimental: { appDir: true , serverComponentsExternalPackages: ["mongoose"],},
+  webpack(config) {
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
