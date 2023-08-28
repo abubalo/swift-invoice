72f6404c0a4936f4d01cc0da7064626ba6f251da
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-08-28 22:32:11 +0200

    Remove dbConnect function invocation

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,4 @@
 import "../globals.css";
-import dbConnect from "../api/utils/db";
 
 export const metadata = {
   title: "Swift invoice",
@@ -12,7 +11,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
 
-  dbConnect();
   return (
     <html lang="en">
       <link rel="shortcut icon" href="/logo.svg" type="image/x-icon" />
