8820f05821a743119237e6eea82aedc7f15f5d71
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-08-31 14:35:55 +0200

    Refactor POST request handling for user addition

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,12 +1,20 @@
-import { NextRequest, NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import { addUser } from "./controller";
 
-export async function POST(request: NextRequest) {
+export async function POST(request: NextRequest): Promise<NextResponse> {
+  try {
+    const requestData = await request.json();
+    const response = await addUser(requestData);
+    const data = await response.json();
 
-  return (await addUser(await request.json()));
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json("Something went wrong!", { status: 500 });
+  }
 }
 
-export async function GET() { 
+export async function GET() {
   return new Response("Hello from get user route", { status: 200 });
 }
 
