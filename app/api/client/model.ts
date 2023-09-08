f1d97c6c85c7d2f544f1ea9ede136972d9d9aad5
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-09-08 18:13:36 +0200

    Fix type error in client model

--- app/api/client/model.ts
+++ app/api/client/model.ts
@@ -1,14 +1,14 @@
-import mongoose from "mongoose";
+import mongoose, {Model} from "mongoose";
 import { ClientDocument } from "@/app/types/types";
 
-const clientSchema = new mongoose.Schema<ClientDocument>({
+const clientSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  billingAddress: { type: String, required: true },
+  address: { type: String, required: true },
 });
 
-const ClientModel: ClientDocument =
+const ClientModel: Model<ClientDocument> =
   mongoose.models?.Client ||
   mongoose.model<ClientDocument>("Client", clientSchema);
 
