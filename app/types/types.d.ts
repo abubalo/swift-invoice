5cc95d366e531433aba3eeb32f9d3d6bc844f172
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-09-08 16:36:51 +0200

    Update types and renamed services to item

--- app/types/types.d.ts
+++ app/types/types.d.ts
@@ -15,38 +15,47 @@ export interface ClientDocument extends Document {
 }
 
 
-export interface IService {
-  serviceName: string;
+export interface ItemDocument {
+  name?: string;
+  description: string
   quantity: number;
-  price: number;
+  unitPrice: number;
+  totalPrice: number;
 }
 
 export interface CompanyDocument {
   user: ObjectId;
-  companyName: string;
+  name: string;
   address: string;
   phone: string;
   email: string;
 }
 
-export interface ICustomer {
-  billTo: string;
-  customerName: string;
-  customerAddress: string;
-  customerEmail: string;
+export interface CustomerDocument {
+  name: string;
+  address: string;
+  email: string;
 }
 
 export interface InvoiceDocument extends Document {
   company: CompanyDocument;
-  customer: ICustomer;
-  services: IService[];
-  issueDate: Date | string;
-  dueDate: Date | string;
-  totalAmount: number;
+  customer: CustomerDocument;
+  Items: ItemDocument[];
+  invoiceNo: string;
+  currency: string;
+  issueDate: Date;
+  dueDate: Date;
+  paymentStatus: "Pending" | "Paid" | "Partially Paid";
+  paymentHistory: {
+    paymentDate: Date;
+    amount: number;
+  }[];
 }
 
+
+
+
 export interface Payment extends Document {
-  _id: ObjectId;
   invoice: ObjectId;
   amount: number;
   paymentDate: Date;
@@ -54,23 +63,20 @@ export interface Payment extends Document {
   status: string;
 }
 
-export interface Settings {
-  _id: ObjectId;
+export interface Settings extends Document {
   user: ObjectId;
   preferredCurrency: string;
   defaultTaxRate: number;
 }
 
-export interface Notification {
-  _id: ObjectId;
+export interface Notification extends Document {
   user: ObjectId;
   recipient: string;
   message: string;
   dateTime: Date;
 }
 
-export interface ActivityLog {
-  _id: ObjectId;
+export interface ActivityLog extends Document{
   user: ObjectId;
   action: string;
   timestamp: Date;
