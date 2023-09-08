0876bb2fa2e64b96a530166d469d88e837feb2c4
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-09-08 23:07:21 +0200

    Add CRUD for invoice

--- app/api/invioce/service.ts
+++ app/api/invioce/service.ts
@@ -1,7 +1,9 @@
-import { IInvoice } from "@/app/types/types";
+import { InvoiceDocument } from "@/app/types/types";
 import InvoiceModel from "./model";
 
-export const addInvoice = async (data: IInvoice): Promise<IInvoice | null> => {
+export const addInvoice = async (
+  data: InvoiceDocument
+): Promise<InvoiceDocument | null> => {
   try {
     const invoice = await InvoiceModel.create(data);
     return invoice;
@@ -11,44 +13,57 @@ export const addInvoice = async (data: IInvoice): Promise<IInvoice | null> => {
 };
 
 export const selectInvoice = async (
-  data: IInvoice
-): Promise<IInvoice | null> => {
+  id: string
+): Promise<InvoiceDocument | null> => {
   try {
-    const invoice = await InvoiceModel.create(data);
+    const invoice = await InvoiceModel.findById(id);
+
+    if (!invoice) {
+      return null;
+    }
+
     return invoice;
   } catch (error) {
+    console.error("Error selecting invoice:", error);
     return null;
   }
 };
 
-export const selectAllInvoices = async (
-  data: IInvoice
-): Promise<IInvoice | null> => {
+export const selectAllInvoices = async (): Promise<
+  InvoiceDocument[] | null
+> => {
   try {
-    const invoice = await InvoiceModel.create(data);
-    return invoice;
+    const invoices = await InvoiceModel.find();
+    return invoices;
   } catch (error) {
     return null;
   }
 };
 
 export const updateInvoice = async (
-  data: IInvoice
-): Promise<IInvoice | null> => {
+  id: string,
+  data: Partial<InvoiceDocument>
+): Promise<InvoiceDocument | null> => {
   try {
-    const invoice = await InvoiceModel.create(data);
+    const invoice = await InvoiceModel.findByIdAndUpdate(id, data, {
+      new: true,
+    });
+
+    if (!invoice) {
+      return null;
+    }
+
     return invoice;
   } catch (error) {
+    console.error("Error updating invoice:", error);
     return null;
   }
 };
 
-export const deleteInvoice = async (
-  data: IInvoice
-): Promise<IInvoice | null> => {
+export const deleteInvoice = async (id: string): Promise<Boolean | null> => {
   try {
-    const invoice = await InvoiceModel.create(data);
-    return invoice;
+    const invoice = await InvoiceModel.findByIdAndDelete(id);
+    return !!invoice;
   } catch (error) {
     return null;
   }
