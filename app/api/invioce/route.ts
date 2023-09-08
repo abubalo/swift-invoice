bae009de603f6ac856110e9e0924b05b56dea6d9
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-09-08 23:08:19 +0200

    Create post endpoint for invoice

--- app/api/invioce/route.ts
+++ app/api/invioce/route.ts
@@ -1,7 +1,36 @@
 import { NextResponse } from "next/server";
+import { createInvoice } from "./controller";
 
+export const POST = async (req: Request) => {
+  try {
+    const data = await req.json();
+    const response = await createInvoice(data);
+    return NextResponse.json(response.json(), { status: 200 });
+  } catch (error) {
+    return NextResponse.json({"error": "Something went, please try again"}, {status: 500})
+  }
+};
 
-export const POST = (req:Request)=>{
+export const GET = async (req: Request) => {
+  try {
+    const requestData = await req.json();
+    const response = await createInvoice(requestData);
+    return NextResponse.json(response.json(), { status: 200 });
+  } catch (error) {}
+};
 
-    NextResponse.json("Hello")
-}
+export const UPDATE = async (req: Request) => {
+  try {
+    const requestData = await req.json();
+    const response = await createInvoice(requestData);
+    return NextResponse.json(response.json(), { status: 200 });
+  } catch (error) {}
+};
+
+export const DELETE = async (req: Request) => {
+  try {
+    const requestData = await req.json();
+    const response = await createInvoice(requestData);
+    return NextResponse.json(response.json(), { status: 200 });
+  } catch (error) {}
+};
