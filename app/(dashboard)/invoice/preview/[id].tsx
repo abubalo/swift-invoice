a220b89150edd2bde4432d04f45a270a56bde142
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-09-08 16:35:48 +0200

    Add page for previewing invoice

---
+++ app/(dashboard)/invoice/preview/[id].tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import InvoiceTemplate from '../../components/InvoiceTemplate'
+
+type Props = {}
+
+function Preview({}: Props) {
+  return (
+    <InvoiceTemplate invoiceId="DA-43925" />
+  )
+}
+
+export default Preview
