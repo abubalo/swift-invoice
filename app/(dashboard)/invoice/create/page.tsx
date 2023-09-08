9966ac75df97025e89c62f917a95709f0a436b4d
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-09-08 16:35:15 +0200

    Add page for create invoice

---
+++ app/(dashboard)/invoice/create/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import InvoiceForm from '../../components/InvoiceForm'
+
+const CreateInvoice = () => {
+  return (
+    <div>
+        <InvoiceForm />
+    </div>
+  )
+}
+
+export default CreateInvoice
