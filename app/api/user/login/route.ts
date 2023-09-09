ac517f8c744a67f4ec2b9f9ce9a6abdf6f9ce9d4
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-09-09 23:58:27 +0200

    Implement login route for user

--- app/api/user/login/route.ts
+++ app/api/user/login/route.ts
@@ -1,7 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import { login } from "../controller";
 
-export async function POST(req: NextRequest) {
-    const data = await req.json()
-    await login(data )
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  try {
+    const requestData = await req.json();
+    const response = await login(requestData);
+    const data = await response.json();
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({error: "something went wrong"}, { status: 200 });
   }
+}
