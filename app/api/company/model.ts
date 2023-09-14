2b1b852f3a2dc0601b8e2d116608f0840b9e17cf
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-09-14 20:27:09 +0200

    Update company schema values to match the types

--- app/api/company/model.ts
+++ app/api/company/model.ts
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
-import { CompanyDocument } from "@/app/types/types";
+import { SellerDocument } from "@/app/types/types";
 
-const CompanySchema = new mongoose.Schema<CompanyDocument>({
+const CompanySchema = new mongoose.Schema<SellerDocument>({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User",  required: true },
-  companyName: { type: String, required: true },
+  name: { type: String, required: true },
   address: { type: String, required: true },
   phone: { type: String },
   email: { type: String, required: true },
 });
 
-const CompanyModel = mongoose.models?.Company || mongoose.model<CompanyDocument>("Company", CompanySchema);
+const CompanyModel = mongoose.models?.Company || mongoose.model<SellerDocument>("Company", CompanySchema);
 
 export default CompanyModel;
