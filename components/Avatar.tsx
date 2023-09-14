025460056ea5ca3ba2bd437a22e82dba0ae20921
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-09-14 18:38:56 +0200

    Add user.name to Avatar component

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,16 +1,21 @@
+import { useAuth } from "@/app/utils/hooks/AuthContext";
 import Image from "next/image";
 import React from "react";
 
 const Avatar = () => {
+  const { user } = useAuth();
   return (
-    <div className="w-max overflow-hidden rounded-full aspect-square">
-      <Image
-        src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"
-        alt="user image"
-        width={50}
-        height={50}
-        className="rounded-full overflow-hidden"
-      />
+    <div>
+      <div className="w-max overflow-hidden rounded-full aspect-square">
+        <Image
+          src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"
+          alt="user image"
+          width={50}
+          height={50}
+          className="rounded-full overflow-hidden"
+        />
+      </div>
+      <p>{user?.name}</p>
     </div>
   );
 };
