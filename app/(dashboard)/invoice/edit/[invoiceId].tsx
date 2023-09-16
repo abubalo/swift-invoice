f49b15dd4aa1821c762bff44e6f75ffb4c65133f
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-09-16 21:28:12 +0200

    Implement edit invoice form

--- app/(dashboard)/invoice/edit/[invoiceId].tsx
+++ app/(dashboard)/invoice/edit/[invoiceId].tsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react'
+import EditInvoiceForm, { InitialValues as FormData } from '../../components/EditInvoiceForm'
 
 type Props = {}
 
 const Edit = (props: Props) => {
+  const router = useRouter();
+  const {invoiceId} = router.query;
+
+  const [data, setData] = useState<FormData| null>(null)
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      const res = await axios.get(`/api/invoices?id=${invoiceId}`);
+      const data = await res.data;
+      setData(data);
+    }
+    fetchData();
+  },[invoiceId])
+
   return (
-    <div>Edit</div>
+    <div>
+      <EditInvoiceForm data={data}/>
+    </div>
   )
 }
 
