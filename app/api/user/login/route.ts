e06eef51f9b1a4d371d5738a079972b90a7b85f1
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-09-19 08:48:10 +0200

    Move login controler logic to route.ts

--- app/api/user/login/route.ts
+++ app/api/user/login/route.ts
@@ -1,13 +1,38 @@
 import { NextRequest, NextResponse } from "next/server";
-import { login } from "../controller";
+import { generateAccessToken } from "../../utils/auth";
+import UserService from "../service";
+import { serialize, parse } from "cookie";
 
-export async function POST(req: NextRequest): Promise<NextResponse> {
+
+export const POST = async (req: NextRequest): Promise<NextResponse> => {
   try {
-    const requestData = await req.json();
-    const response = await login(requestData);
-    const data = await response.json();
-    return NextResponse.json(data, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({error: "something went wrong"}, { status: 200 });
+    const {email, password} = await req.json()
+    const user = await UserService.authenticate(email, password);
+    console.log("USER:", user)
+
+    if (!user) {
+      return NextResponse.json("Invalid login credentials", { status: 401 });
+    }
+
+    const token = await generateAccessToken(user);
+
+    const cookie = serialize("token", token, {
+      httpOnly: true,
+      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+      path: "/",
+    });
+
+    return NextResponse.json(user, {
+      headers: {
+        "Set-Cookie": cookie,
+      },
+    });
+  } catch (error: any) {
+    console.log("Error message: ", error.message)
+    return NextResponse.json(
+      { error: "Something went wrong. Unable to login user" },
+      { status: 500 }
+    );
   }
-}
+};
