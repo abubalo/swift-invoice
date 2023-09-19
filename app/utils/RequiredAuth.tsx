f14da96f2a41b1f6b5ba327c1b3fe170bef0318c
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-09-19 08:58:34 +0200

    Move user conditional statement on to of loading conditional statement

--- app/utils/RequiredAuth.tsx
+++ app/utils/RequiredAuth.tsx
@@ -5,21 +5,21 @@ import { useAuth } from "./hooks/AuthContext";
 import LoadingIndicator from "@/components/ui/LoadingIndicator";
 
 const RequiredAuth = <T extends object>(WrappedComponent: any) => {
-  return function WithAuth (props: T) {
+  return function WithAuth(props: T) {
     const { user, loading } = useAuth();
     const router = useRouter();
 
-    if (loading) {
-        return <LoadingIndicator />;
-      }
-
     if (!user) {
-      router.push("/login");
+      router.replace("/login");
       return null;
     }
+
+    if (loading) {
+      return <LoadingIndicator />;
+    }
+
     return <WrappedComponent {...props} />;
   };
-
 };
 
 export default RequiredAuth;
