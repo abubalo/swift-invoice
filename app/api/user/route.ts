63fad0b60e1697ad167e2ffe5e57bfcfe70650b5
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-09-19 08:47:42 +0200

    Move controler logics to route.ts

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,29 +1,89 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import { addUser } from "./controller";
+import UserService from "./service";
+import { verifyAccessToken } from "../utils/auth";
 
-export async function POST(request: NextRequest): Promise<NextResponse> {
+export const POST = async (req: NextRequest): Promise<NextResponse> => {
   try {
-    const requestData = await request.json();
-    const response = await addUser(requestData);
-    const data = await response.json();
+    const { name, email, password } = await req.json();
 
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json("Something went wrong!", { status: 500 });
+    const isExists = await UserService.verifyUser(email);
+
+    if (isExists) {
+      return NextResponse.json(
+        { message: "User already exists" },
+        { status: 409 }
+      );
+    }
+
+    const user = await UserService.addUser(name, email, password);
+    return NextResponse.json(user);
+  } catch (error: any) {
+    console.log({ error: error.message });
+    return NextResponse.json(
+      { error: "Something went wrong, from controller" },
+      { status: 500 }
+    );
   }
-}
+};
 
-export async function GET() {
-  return new Response("Hello from get user route", { status: 200 });
-}
+export const GET = async (req: NextRequest): Promise<NextResponse> => {
+  try {
+    const token = req.cookies.get("token")?.value;
+    console.log("Token: ", token);
 
-export async function PUT(req: Request) {
-  console.log("Hello");
-  return new Response("Hello", { status: 200 });
-}
+    if (!token) {
+      return NextResponse.json({ error: "Unauthize access" }, { status: 409 });
+    }
 
-export async function DELETE(req: Request) {
-  console.log("Hello");
-  return new Response("Hello", { status: 200 });
-}
+    const decodedToken = await verifyAccessToken(token);
+    const userId = decodedToken?._id;
+
+    const data = await UserService.selectUser(userId);
+
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+};
+
+export const PUT = async (req: NextRequest): Promise<NextResponse> => {
+  try {
+    const { _id, name, email } = await req.json();
+    const user = await UserService.updateUser(_id, { name, email });
+    if (!user) {
+      return NextResponse.json(
+        { message: "User does not exist" },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json(null, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Unable to delete user" },
+      { status: 500 }
+    );
+  }
+};
+
+export const DELETE = async (req: NextRequest): Promise<NextResponse> => {
+  try {
+    const { _id } = await req.json();
+    const user = await UserService.deleteUser(_id);
+    if (!user) {
+      return NextResponse.json(
+        { message: "User does not exist" },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json(null, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Unable to delete user" },
+      { status: 500 }
+    );
+  }
+};
