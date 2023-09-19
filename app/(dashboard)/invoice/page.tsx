7b32bdef11243d95a496fec37ddee835fe85f32b
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-09-19 09:45:58 +0200

    Rename the component

--- app/(dashboard)/invoice/page.tsx
+++ app/(dashboard)/invoice/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Generate = () => {
+const Invoice = () => {
   return <section className="w-full">This Invoice page</section>;
 };
 
-export default Generate;
+export default Invoice;
