7e325efa8036b3ee96bd408b96f28064add10cb7
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2024-01-06 23:04:48 +0200

    Add configuration for remote image

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com"], // Add the domain of the image source here
-  },
-  experimental: { 
-    serverComponentsExternalPackages: ["mongoose"],  
-    serverActions: true,
-  },
-  webpack(config) {
-    config.experiments = { ...config.experiments, topLevelAwait: true };
-    return config;
+    remotePatterns: [
+      {
+        hostname: "images.unsplash.com",
+      },
+    ],
   },
 };
 
