e7655416f528f8680df2d5659f4e008e7648f98c
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2024-01-06 15:55:23 +0200

    feat: Create a reusbale button

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+"use client";
 
-const Button = () => {
+import React, { KeyboardEvent, MouseEvent, ReactNode } from "react";
+import {twMerge} from "tailwind-merge"
+
+type ButtonProps = {
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
+  children: ReactNode;
+  className?: string;
+  [key: string]: any // accept other props
+};
+
+const Button: React.FC<ButtonProps> = ({
+  onClick,
+  children,
+  className,
+  rest,
+}: ButtonProps) => {
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    if (onClick) {
+      onClick(event);
+    }
+  };
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLButtonElement>) => {
+    // Trigger onClick on pressing Enter or Space keys
+    if (event.key === "Enter" || event.key === " ") {
+      event.preventDefault();
+      handleClick(event as unknown as MouseEvent<HTMLButtonElement>);
+    }
+  };
+
   return (
-    <div>Button</div>
-  )
-}
+    <button className={twMerge(`${className} w-full text-white dark:bg-primary bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-primary-700 dark:focus:ring-blue-800`)} onClick={handleClick} onKeyDown={handleKeyDown} {...rest}>
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
