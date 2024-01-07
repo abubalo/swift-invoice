bf4c6b16d1f4e0c842f80aac8b036cf56881e720
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2024-01-07 02:27:06 +0200

    Set generic T to unknown

--- app/utils/RequiredAuth.tsx
+++ app/utils/RequiredAuth.tsx
@@ -1,17 +1,26 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { AuthContextValue, useAuth } from "./hooks/AuthContext";
 import LoadingIndicator from "@/components/ui/LoadingIndicator";
 
-const RequiredAuth = <T extends AuthContextValue>(WrappedComponent: any) => {
+const RequiredAuth = <T = unknown>(WrappedComponent: any) => {
   return function WithAuth(props: T) {
     const { user, loading } = useAuth();
     const router = useRouter();
+    const [redirect, setRedirect] = useState(false);
 
-    if (!user) {
+    useEffect(() => {
+      if (!user && !loading) {
+        // User is not authenticated and loading has finished
+        setRedirect(true);
+      }
+    }, [user, loading]);
+
+    if (redirect) {
       router.replace("/login");
-      return null;
+      return <LoadingIndicator />;
     }
 
     return loading ? <LoadingIndicator /> : <WrappedComponent {...props} />;
